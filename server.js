e6283bb2b3808881dc953ce8c05f6345e762652c
dabui1 <dabui1@myseneca.ca>
2022-09-09 09:05:47 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB322 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: __Dai Anh Bui___ Student ID: _102629219_ Date: _September 9, 2022_
+*
+* Online (Cyclic) URL: _______________________________________________________
+*
+********************************************************************************/ 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
